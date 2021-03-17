201095f4ba5f0bcc5eac9e253c775993608319dc
yeonju626 <yenchuchiang626@gmail.com>
2021-03-17 23:20:25 +1100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -10,10 +10,10 @@ from math import floor, sqrt
 
 def primes(n):
     '''Returns the set of primes between 2 and n, inclusive.'''
-    primes = set(range(2, n+1))
-    for k in range(2, floor(sqrt(n))+1):
+    primes = set(range(2, n+1)) #set contains all the integers between 2 and n
+    for k in range(2, floor(sqrt(n))+1):#for every integer in the range of 2 and square root of n
         if k in primes:
-            primes.difference_update( range(k**2, n+1, k) )
+            primes.difference_update( range(k**2, n+1, k) )#compares the set primes and the set of the integers whose square root is k and can be devided by k (bigger than k)                                   #and take out the repeated integer from primes
     return primes
 
 
